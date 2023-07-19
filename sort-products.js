df0abb334ce269fc0b5e39203b51a6bea083f01c
Lionel Kaneza <lionkaneza@gmail.com>
2023-07-19 18:01:16 +0200

    add sort-products.js file

---
+++ sort-products.js
@@ -0,0 +1,32 @@
+const sortProducts = arr => {
+    return arr.sort((a, b) => a.price - b.price)
+};
+
+const products = [
+    {
+        name: 'orange',
+        price: 15
+    },
+    {
+        name: 'pineapple',
+        price: 10
+    },
+    {
+        name: 'cup',
+        price: 6
+    },
+    {
+        name: 'computer',
+        price: 30
+    },
+    {
+        name: 'phone',
+        price: 13
+    },
+    {
+        name: 'charger',
+        price: 3
+    },
+]
+
+console.log(sortProducts(products))
