21e70a5b381fd78aff574c93f3a549e27f5f1116
Lionel Kaneza <lionkaneza@gmail.com>
2023-07-19 14:57:09 +0200

    tweak reverse-in-place logic

--- reverse-in-place.js
+++ reverse-in-place.js
@@ -2,17 +2,7 @@ const reverse = (str) => {
   str = str.split(" ");
   let newArr = [];
   for (let sub of str) {
-    if (sub.length > 1) {
-      sub = sub.split("");
-      for (let j = 0; j < sub.length / 2; j++) {
-        let temp = sub[j];
-        sub[j] = sub[sub.length - 1 - j];
-        sub[sub.length - 1 - j] = temp;
-      }
-      newArr.push(sub.join(""));
-    } else {
-      newArr.push(sub);
-    }
+    newArr.push(sub.split("").reverse().join(""));
   }
   return newArr.join(" ");
 };
