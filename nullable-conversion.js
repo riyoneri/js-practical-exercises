6f68c946bd65fd072e18280aaf2ace447bbb1b42
Lionel Kaneza <lionkaneza@gmail.com>
2023-07-19 20:19:18 +0200

    add nullable-conversion.js

---
+++ nullable-conversion.js
@@ -0,0 +1,21 @@
+const convertNullableValues = (obj) => {
+  for (let prop in obj) {
+    if (obj[prop] === null) obj[prop] = 0;
+    if (obj[prop] === undefined) obj[prop] = "";
+  }
+
+  return obj;
+};
+
+const myObj = {
+  name: "lion",
+  age: 54,
+  car: true,
+  level: 5,
+  weight: undefined,
+  color: "white",
+  hair: null,
+  computer: "lenovo",
+};
+
+console.log(convertNullableValues(myObj));
