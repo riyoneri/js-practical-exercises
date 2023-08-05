deb9ba4b3cf006e4307c5b14b26a7dccc5bda36c
Lionel Kaneza <lionkaneza@gmail.com>
2023-08-05 19:01:11 +0200

    birdsInWeek[bird-watcher]

--- ES6/bird-watcher.js
+++ ES6/bird-watcher.js
@@ -5,5 +5,15 @@ function totalBirdCount(birdsPerDay) {
   console.log(sum);
 }
 
+function birdsInWeek(birdsInWeek, weekNumber) {
+    let sum = 0;
+    for(let i = (7*(weekNumber-1)); i<(weekNumber * 7); i++) sum+= birdsInWeek[i]
+
+    console.log(sum)
+}
+
 const birdsPerDay = [2, 5, 0, 7, 4, 1, 3, 0, 2, 5, 0, 1, 3, 1];
-totalBirdCount(birdsPerDay);
+
+
+// totalBirdCount(birdsPerDay);
+birdsInWeek(birdsPerDay, 2)
