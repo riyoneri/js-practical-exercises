574686b43d59ef0f1e20ccc6ab52358dc5699fee
Lionel Kaneza <lionkaneza@gmail.com>
2023-08-06 18:14:27 +0200

    Position class[windowing-system]

--- ES6/windowing-system.js
+++ ES6/windowing-system.js
@@ -1,14 +1,27 @@
-class Size{
-    constructor(width=60, height=60){
-        this.width = width
-        this.height = height
-    }
+class Size {
+  constructor(width = 60, height = 60) {
+    this.width = width;
+    this.height = height;
+  }
 
-    resize(width, height) {
-        this.width = width
-        this.height = height
-    }
+  resize(width, height) {
+    this.width = width;
+    this.height = height;
+  }
+}
+
+class Position {
+  constructor(x = 0, y = 0) {
+    this.x = x;
+    this.y = y;
+  }
+
+  move(x, y) {
+    this.x = x;
+    this.y = y;
+  }
 }
 
 const size = new Size(1);
-console.log(size.width)
+const point = new Position();
+console.log(point.x, point.y);
